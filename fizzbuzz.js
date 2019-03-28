0754e1fa2a3950941f075339740fcdb07edafb12
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-03-28 23:21:58 +0530

    modified

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,4 @@
+//With While Loop
 function fizz(N){
 while(N>=1){
 if(N%3==0){
@@ -10,3 +11,15 @@ N--
 }
 }
 fizz(100)
+//With for loop
+function Buzz(N){
+    for(i=0;i<=N;i++){
+        if(i%3==0){
+            console.log("Fizz");
+        }
+        else if(i%5==0){
+            console.log("Buzz");
+        }
+    }
+}
+Buzz(100)
