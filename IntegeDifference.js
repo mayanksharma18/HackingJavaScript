6a14970bf35ab339cd225963f42158ac518bfc6b
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-03-28 22:34:10 +0530

    print prime

---
+++ IntegeDifference.js
@@ -0,0 +1,8 @@
+//decrement  the first number by 1 upto count of second number
+function diff(x,y){
+    for(i=1;i<=y;i++){
+      x--;
+    } 
+    return x;
+    }
+    diff(12,5)
--- prime.js
+++ prime.js
@@ -1,5 +1,5 @@
 function Prime(n){
-    if(n==1||n==2||n==3||n==5||n==7){
+    if(n==2||n==3||n==5||n==7){
     console.log( n+" "+"number is prime")
     }
     else if(n%2==0||n%3==0||n%5==0||n%7==0){
