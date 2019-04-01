01a580d99334582fda330a39ec3ff5fb28269e75
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-04-01 09:57:26 +0530

    arrayswapping

---
+++ Arrayswap.js
@@ -0,0 +1,17 @@
+//Swapping of Arrays of any length
+function swap(x,y){
+    if(x.length<y.length){
+      n=x.length;
+    }
+    else{
+      n=y.length;
+    }
+    for(i=0;i<n;i++){
+      z=x[i]
+      x[i]=y[i]
+      y[i]=z
+    }
+  console.log(x,y)
+  }
+  swap([1,45,7,9,6],[0,0,0,0,0,0,0,0]);
+  
